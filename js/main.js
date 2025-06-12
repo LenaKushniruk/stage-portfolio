0e45128496961e2f49941e34d94d417ce5a3e70a
Lena Kushniruk <34978@ma-web.nl>
2025-06-12 13:59:32 +0200

    aangepast

---
+++ js/main.js
@@ -0,0 +1,15 @@
+document.getElementById('aboutme-button').addEventListener('click', () => {
+  document.getElementById('aboutme').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('skills-button').addEventListener('click', () => {
+  document.getElementById('skills').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('projects-button').addEventListener('click', () => {
+  document.getElementById('projects').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('contact-button').addEventListener('click', () => {
+  document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
+});
